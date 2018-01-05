28248d6b8b28f3fbbee0b55d9ab3c336c90242c5
cgn <sdjzgw@163.com>
2018-01-05 19:08:05 +0800

    stencil mpi primary version

--- mpi/stencil_mpi.cpp
+++ mpi/stencil_mpi.cpp
@@ -14,12 +14,15 @@
 typedef double FT;
 typedef double* PFT;
 
+#define PRINT_DEBUG
+//define INTEL_VAR
 #define CENTER_WEIGHT 1.0
 #define ADJACENT_WEIGHT 0.1
 #define MASTER 0
 #define DATA_TAG 1
+#define GATHER_TAG 2
 
-#define E(data, x, y, z, xtride, ystride) data[((z)*(ystride)+y) * (xtride) + x]
+#define E(data, x, y, z, xstride, ystride) data[((z)*(ystride)+y) * (xstride) + x]
 
 
 void init_data(PFT data, size_t Nx, size_t Ny, size_t Nz)
@@ -28,7 +31,7 @@ void init_data(PFT data, size_t Nx, size_t Ny, size_t Nz)
 	for(z = 1; z <= Nz; ++z)
 		for(y = 1; y <= Ny; ++y)
 			for(x = 1; x <= Nx; ++x)
-				E(data, x, y, z, Nx, Ny) = 1.0;
+				E(data, x, y, z, Nx+2, Ny+2) = 1.0;
 }
 
 void stencil_7_point(PFT input_data, PFT output_data, int height, int Nx, int Ny)
@@ -60,10 +63,27 @@ void stencil_7_point(PFT input_data, PFT output_data, int height, int Nx, int Ny
 	}
 }
 
+void print_data(PFT data, int Nx, int Ny, int Nz, size_t xstride, size_t ystride)
+{
+	size_t x, y, z;
+	for(z = 0; z < (size_t)Nz; ++z)
+	{
+		printf("\nz : %ld\n", z);
+		for(y = 0; y < (size_t)Ny; ++y)
+		{
+			for(x = 0; x < (size_t)Nx; ++x)
+			{
+				printf("%.3f ",E(data, x+1, y+1, z+1, xstride, ystride));
+			}
+			printf("\n");
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
 
-	int Nx = 100, Ny = 100, Nz = 100, NIter = 100;
+	int Nx = 10, Ny = 10, Nz = 10, NIter = 1;
 	size_t xstride = Nx+2, ystride = Ny+2, zstride = Nz+2;
 	int rank, size;
 	int i, j, BH, LH;
@@ -71,7 +91,10 @@ int main(int argc, char* argv[])
 	PFT data = NULL;
 	PFT data1 = NULL;
 	PFT data2 = NULL;
+	PFT pd1 = NULL;
+	PFT pd2 = NULL;
 	PFT res_data = NULL;
+	PFT tmp = NULL;
 
 	MPI_Status status;
 	MPI_Request request; 
@@ -84,8 +107,13 @@ int main(int argc, char* argv[])
 
 	if(rank == MASTER) // master
 	{
+#ifdef INTEL_VAR
 		data = (PFT)_mm_malloc(memsize, 32);
 		data2 = (PFT)_mm_malloc(xstride*ystride*(BH+2)*sizeof(FT), 32);
+#else
+		data = (PFT)malloc(memsize);
+		data2 = (PFT)malloc(xstride*ystride*(BH+2)*sizeof(FT));
+#endif
 		memset(data, 0, memsize);
 		memset(data2, 0, memsize);
 		init_data(data, (size_t)Nx, (size_t)Ny, (size_t)Nz);
@@ -93,36 +121,104 @@ int main(int argc, char* argv[])
 		for(i = 1; i < size; ++i)
 		{
 			height = (i != size-1) ? BH : LH;
-			MPI_send(&(E(data, 0, 0, i*BH, xstride, ystride)), (height+2)*xstride*ystride, MPI_DOUBLE, i, DATA_TAG, MPI_COMM_WORLD);
+			MPI_Send(&(E(data, 0, 0, i*BH, xstride, ystride)), (height+2)*xstride*ystride, MPI_DOUBLE, i, DATA_TAG, MPI_COMM_WORLD);
 		}
 		data1 = data;
+		pd1 = data1;
+		pd2 = data2;
 		height = BH;
-		stencil_7_point(data1, data2, height, Nx, Ny);
+		stencil_7_point(pd1, pd2, height, Nx, Ny);
 
 		for(int iter = 0; iter < NIter-1; ++ iter)
 		{
 			//send the top layer exactly under the ghost layer
-			MPI_Isend(&(E(data2, 0, 0, height, xstride, ystride)), xstride*ystride, MPI_DOUBLE, MASTER+1, DATA_TAG, MPI_COMM_WORLD, &request);
+			MPI_Isend(&(E(pd2, 0, 0, height, xstride, ystride)), xstride*ystride, MPI_DOUBLE, MASTER+1, DATA_TAG, MPI_COMM_WORLD, &request);
 			//recv the ghost layer
-			MPI_Recv(&(E(data2, 0, 0, height+1, xstride, ystride)), xstride*ystride, MPI_DOUBLE, MASTER+1, DATA_TAG, MPI_COMM_WORLD, &status);
+			MPI_Recv(&(E(pd2, 0, 0, height+1, xstride, ystride)), xstride*ystride, MPI_DOUBLE, MASTER+1, DATA_TAG, MPI_COMM_WORLD, &status);
 			MPI_Wait(&request, MPI_STATUS_IGNORE);
 			
+			//swap
+			tmp = pd1;
+			pd2 = pd1;
+			pd1 = tmp;
+			stencil_7_point(pd1, pd2, height, Nx, Ny);
 		}
+
+		memcpy(&(E(data, 0,0,1, xstride, ystride)), &(E(pd2, 0,0,1, xstride, ystride)), BH*xstride*ystride*sizeof(FT));
+		//gather all the results
+		for(i = 1; i < size; ++i)
+		{
+			height = (i != size-1) ? BH : LH;
+			MPI_Recv(&(E(data, 0, 0, i*BH+1, xstride, ystride)), height*xstride*ystride, MPI_DOUBLE, i, GATHER_TAG, MPI_COMM_WORLD, &status);
+			printf("Finish gather data from process %d, size : %ld\n", i, height*xstride*ystride);
+		}
+
+#ifdef PRINT_DEBUG
+		print_data(data, Nx, Ny, Nz, xstride, ystride);
+#endif
+
+#ifdef INTEL_VAR
+		_mm_free(data);
+		_mm_free(data2);
+#else
+		free(data);
+		free(data2);
+#endif
 
 	}
 	else
 	{
 		height = (rank != size-1) ? BH : LH;
 		size_t blocksize = xstride*ystride*(height+2);
+#ifdef INTE_VAR
 		data1 = (PFT)_mm_malloc(blocksize * sizeof(FT), 32);
-		memset(data1, 0, blocksize * sizeof(FT));
 		data2 = (PFT)_mm_malloc(blocksize * sizeof(FT), 32);
+#else
+		data1 = (PFT)malloc(blocksize * sizeof(FT));
+		data2 = (PFT)malloc(blocksize * sizeof(FT));
+#endif
+		memset(data1, 0, blocksize * sizeof(FT));
 		memset(data2, 0, blocksize * sizeof(FT));
 
-		MPI_Recv(data1, blocksize, MPI_DOUBLE, MASTER, DATA_TAG, MPI_COMM_WORLD, &status);
-		stencil_7_point(data1, data2, height, Nx, Ny);
+		pd1 = data1;
+		pd2 = data2;
+		MPI_Recv(pd1, blocksize, MPI_DOUBLE, MASTER, DATA_TAG, MPI_COMM_WORLD, &status);
+		stencil_7_point(pd1, pd2, height, Nx, Ny);
 
-		for()
+		for(int iter = 0; iter < NIter-1; ++ iter)
+		{
+			if(rank != size-1) // the top block no need to exchange the top ghost layer
+			{
+				//send the top layer exactly under the ghost layer
+				MPI_Isend(&(E(pd2, 0, 0, height, xstride, ystride)), xstride*ystride, MPI_DOUBLE, rank+1, DATA_TAG, MPI_COMM_WORLD, &request);
+				//recv the ghost layer
+				MPI_Recv(&(E(pd2, 0, 0, height+1, xstride, ystride)), xstride*ystride, MPI_DOUBLE, rank+1, DATA_TAG, MPI_COMM_WORLD, &status);
+				MPI_Wait(&request, MPI_STATUS_IGNORE);
+			}
+
+			//send the bottom layer exactly above the ghost layer
+			MPI_Isend(&(E(pd2, 0, 0, 1, xstride, ystride)), xstride*ystride, MPI_DOUBLE, rank-1, DATA_TAG, MPI_COMM_WORLD, &request);
+			MPI_Recv(&(E(pd2, 0, 0, 0, xstride, ystride)), xstride*ystride, MPI_DOUBLE, rank-1, DATA_TAG, MPI_COMM_WORLD, &status);
+			MPI_Wait(&request, MPI_STATUS_IGNORE);
+
+			//swap
+			tmp = pd1;
+			pd2 = pd1;
+			pd1 = tmp;
+
+			stencil_7_point(pd1, pd2, height, Nx, Ny);
+
+		}
+
+		//send to master
+		MPI_Send(&(E(pd2, 0, 0, 1, xstride, ystride)), height*xstride*ystride, MPI_DOUBLE, MASTER, GATHER_TAG, MPI_COMM_WORLD);
+#ifdef INTEL_VAR
+		_mm_free(data1);
+		_mm_free(data2);
+#else
+		free(data1);
+		free(data2);
+#endif
 
 	}
 
